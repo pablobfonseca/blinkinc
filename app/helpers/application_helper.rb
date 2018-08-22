e268d8c1fa97900c80f784866b50ca6dacb4b6b8
Pablo Fonseca <pablofonseca777@gmail.com>
2018-08-22 11:25:31 -0300

    add method to display presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def current_account
+    current_user || current_admin_user
+  end
+
+  def present(model, presenter_class=nil)
+    klass = presenter_class || "#{model.class}Presenter".constantize
+    presenter = klass.new(model, self)
+    block_given?? yield(presenter) : presenter
+  end
 end
